dc2dc923035e7957d5ddadb534d7396d52232275
yarikmuzyka <y.myzuka@gmail.com>
2026-07-07 18:14:50 +0300

    Simplify new-coffee form to essential fields only

--- app/coffees/new/page.jsx
+++ app/coffees/new/page.jsx
@@ -1,9 +1,7 @@
 import Link from 'next/link'
 import { prisma } from '../../../lib/prisma.js'
 import { createCoffee } from '../../../lib/actions.js'
-import {
-  ORIGIN_COUNTRIES, VARIETIES, ROAST_LEVELS,
-} from '../../../lib/constants.js'
+import { ORIGIN_COUNTRIES, VARIETIES } from '../../../lib/constants.js'
 
 export const dynamic = 'force-dynamic'
 
@@ -64,78 +62,18 @@ export default async function NewCoffeePage() {
 
         <div className="row-3">
           <div className="field">
-            <label>Ферма</label>
-            <input name="farm" />
-          </div>
-          <div className="field">
-            <label>Виробник</label>
-            <input name="producer" />
-          </div>
-          <div className="field">
             <label>Обробка</label>
             <input name="process" list="processes" placeholder="washed" />
             <Datalist id="processes" options={processes.map((p) => p.name)} />
-          </div>
-        </div>
-
-        <div className="row-3">
-          <div className="field">
-            <label>Рівень обсмаження</label>
-            <input name="roastLevel" list="roasts" placeholder="light" />
-            <Datalist id="roasts" options={ROAST_LEVELS} />
-          </div>
-          <div className="field">
-            <label>Дата обсмаження</label>
-            <input type="date" name="roastDate" />
           </div>
           <div className="field">
             <label>Дата купівлі</label>
             <input type="date" name="purchaseDate" />
           </div>
-        </div>
-
-        <div className="row-3">
           <div className="field">
             <label>Ціна (₴)</label>
             <input type="number" step="0.01" name="price" placeholder="420" />
           </div>
-          <div className="field">
-            <label>Вага (г)</label>
-            <input type="number" name="weightGrams" placeholder="250" />
-          </div>
-          <div className="field">
-            <label>Ціна / 100г</label>
-            <input disabled placeholder="рахується автоматично" />
-          </div>
-        </div>
-
-        <div className="field">
-          <label>Заявлені ноти (від обсмажчика)</label>
-          <input name="declaredNotes" placeholder="jasmine, bergamot, lemon" />
-        </div>
-        <div className="field">
-          <label>Власні нотатки</label>
-          <textarea name="userNotes" rows={2} />
-        </div>
-
-        <div className="row">
-          <div className="field">
-            <label>Посилання на товар</label>
-            <input name="productUrl" type="url" placeholder="https://…" />
-          </div>
-          <div className="field">
-            <label>Фото пакету (URL)</label>
-            <input name="imageUrl" type="url" placeholder="https://…" />
-          </div>
-        </div>
-
-        <div className="row">
-          <label className="field checkbox">
-            <input type="checkbox" name="isFinished" value="true" /> Вже завершена
-          </label>
-          <label className="field checkbox">
-            <input type="checkbox" name="wouldBuyAgain" value="true" /> Купив би ще раз
-          </label>
         </div>
 
         <div style={{ display: 'flex', gap: 10 }}>
